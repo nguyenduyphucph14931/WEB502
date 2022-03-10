7f9dbd6f0ba0a02861830c09fd953fc90499835f
Phuc ND <phucnd@gmail.com>
2022-03-10 12:23:31 +0700

    commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const a = 20;
+const b = 20;
+const name = "Nguyen Duy Phuc";
+const age = 20;
+const status = true;
+const info = { id: 1, name: "a" };
+const stringArr = ["a", "b", "c"];
+const numberArr = [1, 2, 3, 4, 5];
+const objectArr = [{ id: 1 }, { id: 2 }];
+function sum(numA, numB) {
+    return numA + numB;
+    // Return numA + numB;
+}
+sum(a, b);
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+export{}
+
+
+
+const a: number = 20;
+const b: number = 20;
+const name: string = "Nguyen Duy Phuc";
+const age: number=20;
+const status: boolean = true;
+const info: {id : number , name : string}={id: 1, name :"a"}
+const stringArr: string[] = ["a", "b", "c"];
+const numberArr: number[] = [1,2,3,4,5];
+const objectArr: object[] = [{id: 1}, {id: 2}]
+
+
+function sum(numA : number, numB: number): number{
+    return numA + numB;
+    // Return numA + numB;
+}
+sum(a,b);
